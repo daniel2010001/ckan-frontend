bfb192775a8efd7f68e9347d3fe13e8578b272d9
daniel2010001 <daniel.boris.limachi.cuiza@gmail.com>
2024-12-15 02:02:20 -0400

    Add: models

--- src/models/index.ts
+++ src/models/index.ts
@@ -3,3 +3,5 @@ export * from "./notice.model";
 export * from "./route.model";
 export * from "./storage.model";
 export * from "./store.model";
+export type TypeWithKey<T> = { [key: string]: T };
+export type ValueOf<T> = T[keyof T];
